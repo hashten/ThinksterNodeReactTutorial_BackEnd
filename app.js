df2c2c4df45b8faa6a6d8c6204a62204e9ad7cf0
Joel Harsten <joel.harsten@omegapoint.se>
2019-12-21 18:32:53 +0100

    Authentication Routes set and tested with Postman

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ var fs = require('fs'),
     errorhandler = require('errorhandler'),
     mongoose = require('mongoose');
 
+
 var isProduction = process.env.NODE_ENV === 'production';
 
 // Create global app object
 var app = express();
+
 
 app.use(cors());
 
@@ -38,6 +40,8 @@ if(isProduction){
   mongoose.set('debug', true);
 }
 
+require('./models/User');
+require('./config/passport');
 app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
