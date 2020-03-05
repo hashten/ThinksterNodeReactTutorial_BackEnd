8d8b91d1aaf1005a0478b5a62d3cd049720b0c9f
Joel Harsten <joel.harsten@omegapoint.se>
2020-03-06 00:10:37 +0100

    Change back to environment variables

--- app.js
+++ app.js
@@ -14,16 +14,7 @@ var fs = require('fs'),
 var connectionString = "mongodb://account:key@account.documents.azure.com:10255/?ssl=true";
 var isProduction = process.env.NODE_ENV === 'production';
 if(isProduction){
-  var connectionString = obj.connectionString;
-  var stringSplit1 = connectionString.split("://")[1];
-  var stringSplit2 = stringSplit1.split('@');
-  var userNamePassword = stringSplit2[0];
-  userNamePassword = userNamePassword.split(':');
-  var userName = userNamePassword[0];
-  var password = userNamePassword[1];
-  var databaseName = obj.databaseName;
-  var collectionName = obj.collectionName;
-  connectionString = ("mongodb://" + encodeURIComponent(userName) + ":" + encodeURIComponent(password) + "@" + stringSplit2[1]);
+  var connectionString = process.env.CONNECTIONSTRING;
 }
 
 // Create global app object
